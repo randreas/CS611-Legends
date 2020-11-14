7ecd447f646beb3904af2df4a782d9815b1d0f4c
Richard Andreas <randreas7296@gmail.com>
2020-11-14 13:18:44 +0700

    new valor classes

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
 	 * Main function to create a LegendsGame instance and play the game.
 	 */
 	public static void main(String[] args) {
-		LegendsGame legends = new LegendsGame();
-		legends.startGame();
+//		LegendsGame legends = new LegendsGame();
+//		legends.startGame();
+		ValorGame valor = new ValorGame();
+		valor.startGame();
 	}
 }
---
+++ src/ValorGame.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValorGame extends RPGGame {
+	
+	private List<Hero> warriorList;
+	private List<Hero> paladinList;
+	private List<Hero> sorcererList;
+	private List<Monster> monsters;
+	private Inventory itemInven; 
+	private ioUtility io;
+	private List<LegendsPlayer> playerList;
+	private int numHeroesTeam = 3;
+	private List<String> iconList;
+	
+	
+	public void startGame() {
+		
+		
+//		setPlayers();
+//		
+//		gameMovement();
+//		io.playSound("ending");
+//		System.out.println("Thank you for playing! See you soon!");
+//		io.printASCIIArt("GOODBYE");
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		io.stopAudio();
+	}
+	
+	public ValorGame() {
+		
+		super(null,"ValorGame",1);
+		io = new ioUtility();
+		io.printWelcomeMessage();
+		io.playSound("opening");
+		iconList = new ArrayList<String>();
+		//adding default icons 
+		iconList.add("M");
+		iconList.add("#");
+		iconList.add(" ");
+		Parser p = new Parser();
+		warriorList = p.parseWarriors();
+		paladinList = p.parsePaladins();
+		sorcererList = p.parseSorcerers();
+		monsters = p.parseMonsters();
+		
+		
+		
+		
+		
+	}
+
+}
